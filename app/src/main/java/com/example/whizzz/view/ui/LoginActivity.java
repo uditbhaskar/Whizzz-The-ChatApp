f57783e664d6906097e031c4bdff629bef41f772
me_udit <bhaskarudit8@gmail.com>
2020-03-31 16:17:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/whizzz/view/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.whizzz.view.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.whizzz.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
