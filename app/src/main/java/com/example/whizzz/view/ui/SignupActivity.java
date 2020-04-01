45cc8f0e0ff6880eeeef2653002483d50f13fdf8
me_udit <bhaskarudit8@gmail.com>
2020-04-01 15:47:09 +0530

    SignUp Activity added.

---
+++ app/src/main/java/com/example/whizzz/view/ui/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.whizzz.view.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.whizzz.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
