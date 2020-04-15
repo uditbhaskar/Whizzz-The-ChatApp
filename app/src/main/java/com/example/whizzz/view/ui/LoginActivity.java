6e80fe488e0c4ebd0e0b3baba51fcea5649229cc
me_udit <bhaskarudit8@gmail.com>
2020-04-15 20:31:57 +0530

    Authentication complete.

--- app/src/main/java/com/example/whizzz/view/ui/LoginActivity.java
+++ app/src/main/java/com/example/whizzz/view/ui/LoginActivity.java
@@ -35,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         init();
 
         listener();
