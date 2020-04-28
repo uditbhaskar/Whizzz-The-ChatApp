c55f49387902df0ec17d23a69dbde62edfdacb17
me_udit <bhaskarudit8@gmail.com>
2020-04-28 15:28:17 +0530

    Pager and fragments added.

---
+++ app/src/main/java/com/example/whizzz/view/fragments/UserFragment.java
@@ -0,0 +1,26 @@
+package com.example.whizzz.view.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.whizzz.R;
+
+public class UserFragment extends Fragment {
+   private Context context;
+
+    public UserFragment(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+}
