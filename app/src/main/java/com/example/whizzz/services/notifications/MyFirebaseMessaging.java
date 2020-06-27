a786af7569e0a3eb638b5fcd7fbb9229cdf0a436
me_udit <bhaskarudit8@gmail.com>
2020-06-28 04:01:14 +0530

    Final commit.

--- app/src/main/java/com/example/whizzz/services/notifications/MyFirebaseMessaging.java
+++ app/src/main/java/com/example/whizzz/services/notifications/MyFirebaseMessaging.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.location.OnNmeaMessageListener;
 import android.media.RingtoneManager;
@@ -63,19 +64,23 @@ public class MyFirebaseMessaging extends FirebaseMessagingService implements Lif
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         String sent = remoteMessage.getData().get("sent");
+        String user = remoteMessage.getData().get("user");
+
+        SharedPreferences preferences = getSharedPreferences("PREFS", MODE_PRIVATE);
+        String currentUser = preferences.getString("currentuser", "none");
 
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = mAuth.getCurrentUser();
 
         assert sent != null;
         if (firebaseUser != null && sent.equals(firebaseUser.getUid())) {
-
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-                sendOreoNotification(remoteMessage);
+            if (!currentUser.equals(user)) {
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+                    sendOreoNotification(remoteMessage);
+                }
+                sendNotification(remoteMessage);
             }
-            sendNotification(remoteMessage);
         }
-
 
     }
 
