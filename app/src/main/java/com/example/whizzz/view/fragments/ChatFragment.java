b6af3fad14432fcd458d3cccd30aac2bc58b29f1
me_udit <bhaskarudit8@gmail.com>
2020-08-15 12:04:09 +0530

    Final commit.

--- app/src/main/java/com/example/whizzz/view/fragments/ChatFragment.java
+++ app/src/main/java/com/example/whizzz/view/fragments/ChatFragment.java
@@ -3,7 +3,6 @@ package com.example.whizzz.view.fragments;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
